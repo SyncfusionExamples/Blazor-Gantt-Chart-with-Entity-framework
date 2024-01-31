714c2156276c30c0a40280a4273c98bde24d4990
BackiaRaj <backiaraj.kanagaraj@syncfusion.com>
2024-01-31 14:26:24 +0530

    Update the project

---
+++ Models/EfContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace GanttEF.Models;
+
+public partial class EfContext : DbContext
+{
+    public EfContext()
+    {
+    }
+
+    public EfContext(DbContextOptions<EfContext> options)
+        : base(options)
+    {
+    }
+
+
+    public virtual DbSet<TaskDatum> TaskData { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=EF;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+        modelBuilder.Entity<TaskDatum>(entity =>
+        {
+            entity.HasKey(e => e.TaskId);
+
+            entity.Property(e => e.TaskId)
+                .ValueGeneratedNever()
+                .HasColumnName("TaskID");
+            entity.Property(e => e.EndDate).HasColumnType("datetime");
+            entity.Property(e => e.StartDate).HasColumnType("datetime");
+            entity.Property(e => e.TaskName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
